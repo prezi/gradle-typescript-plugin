675b672ae36e60a8b358c57411cbc3b3a508e102
Lorant Pinter <lorant@gradle.com>
2015-06-22 14:00:43 +0200

    Add source sets

---
+++ src/main/java/com/prezi/typescript/gradle/DefaultTypeScriptLanguageSourceSet.java
@@ -0,0 +1,6 @@
+package com.prezi.typescript.gradle;
+
+import org.gradle.language.base.sources.BaseLanguageSourceSet;
+
+public class DefaultTypeScriptLanguageSourceSet extends BaseLanguageSourceSet implements TypeScriptLanguageSourceSet {
+}
---
+++ src/main/java/com/prezi/typescript/gradle/TypeScriptLanguageSourceSet.java
@@ -0,0 +1,6 @@
+package com.prezi.typescript.gradle;
+
+import org.gradle.language.base.LanguageSourceSet;
+
+public interface TypeScriptLanguageSourceSet extends LanguageSourceSet {
+}
